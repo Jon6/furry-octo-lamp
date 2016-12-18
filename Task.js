3eae8b84f430781fc62930cd4f515403bdfb5d1d
Jon Liang <jonliang0@gmail.com>
2016-12-18 14:09:55 -0600

    no reason

--- Task.js
+++ Task.js
@@ -1,9 +1,1 @@
-//console.log('\nWelcome to CLITask. \n\t1 - Enter a Task\n\t2 - List all Tasks\n');
-
-
-$(document).ready(function() {
-    $('#addTask').submit(function(e) {
-        e.preventDefault();
-        addTask();
-    });
-});
+const express = require('express');
