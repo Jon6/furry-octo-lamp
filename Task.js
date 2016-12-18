21ae929b7cc8e0383b716724c88dc64e497b9f12
Jon Liang <jonliang0@gmail.com>
2016-12-18 14:15:58 -0600

    update file structure AGAIN

--- Task.js
+++ Task.js
@@ -1,1 +1,10 @@
 const express = require('express');
+
+var app = express();
+var server = app.listen(3000, listening);
+
+function listening() {
+    console.log('Listening');
+}
+
+app.use(express.static('public'));
