98935e1923c3b2647b1cf5b5d28356b386ebf9ed
Jon Liang <jonliang0@gmail.com>
2016-12-18 14:03:55 -0600

    updating to separate server/client

--- Task.js
+++ Task.js
@@ -1,1 +1,9 @@
 //console.log('\nWelcome to CLITask. \n\t1 - Enter a Task\n\t2 - List all Tasks\n');
+
+
+$(document).ready(function() {
+    $('#addTask').submit(function(e) {
+        e.preventDefault();
+        addTask();
+    });
+});
