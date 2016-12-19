15bd7cf4eb44bb2e67b685c1178c42e90823ed90
Jon Liang <jonliang0@gmail.com>
2016-12-18 21:47:02 -0600

    adding basic task fucntionality

--- Task.js
+++ Task.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+var tasks = ['Eat something', 'Go to the Gym'];
+
 var app = express();
 var server = app.listen(3000, listening);
 
@@ -7,4 +9,15 @@ function listening() {
     console.log('Listening...');
 }
 
+//to be expanded upon
 app.use(express.static('public'));
+
+app.get('/getTasks/:taskNum', returnTasks);
+
+function returnTasks(request, response) {
+    if (request.params.taskNum !== null) {
+        response.send(tasks[taskNum]);
+    } else {
+        response.send(tasks);
+    }
+}
