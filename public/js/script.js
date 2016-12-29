97841a1781200687feb5efa9765b63b87e4c9e60
Jon Liang <jonliang0@gmail.com>
2016-12-28 21:33:58 -0600

    Changing CSS main-font

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-var btstrpTrash = 'col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2';
+var btstrpClasses = 'col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2';
 
 $(document).ready(function() {
 
@@ -13,7 +13,7 @@ $(document).ready(function() {
   $.getJSON('/getTasks/', function (data){
     for (var i = 0; i < data.length; i++) {
       // $('#main-container').append('<div><p>' + data[i] + '</p></div>');
-      $('<div><p class="main-font">- ' + data[i] + '</p></div>').addClass(btstrpTrash).appendTo('#main-container');
+      $('<div><p class="main-font">- ' + data[i] + '</p></div>').addClass(btstrpClasses).appendTo('#main-container');
     }
   });
 });
