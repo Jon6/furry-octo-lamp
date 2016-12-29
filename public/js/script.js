61470c8a8e699fd01f3c98e7d76cc12c26e47910
Jon Liang <jonliang0@gmail.com>
2016-12-28 21:52:51 -0600

    No re-load when entering a task

--- public/js/script.js
+++ public/js/script.js
@@ -5,27 +5,29 @@ $(document).ready(function() {
   // when form is submitted, send task to add to list
   $('#addTaskForm').submit(function(e) {
     // used to prevent reloading
-    // e.preventDefault();
+    e.preventDefault();
     sendTask();
   });
 
   // load tasks that are in json file with getJSON
-  $.getJSON('/getTasks/', function (data){
+  $.getJSON('/getTasks/', function(data) {
     for (var i = 0; i < data.length; i++) {
       // $('#main-container').append('<div><p>' + data[i] + '</p></div>');
-      $('<div><p class="main-font">- ' + data[i] + '</p></div>').addClass(btstrpClasses).appendTo('#main-container');
+      addToMain(data[i]);
     }
   });
 });
 
 function sendTask() {
-  // var taskStringURL = '/addTask/' + encodeURI($('#taskString').val());
-  // console.log($('#taskString').val());
-  // $.getJSON(taskStringURL, function(data) {
-  //   console.log(data);
-  // });
+  // send to API to add to file and add to DOM so reloading isn't necessary
   var taskString = $('#taskString').val();
   $.post('/postTask/', { text: taskString }, function(data) {
     console.log(data);
   });
+  addToMain(taskString);
+  $('#taskString').val(""); // clear input
+}
+
+function addToMain(str) {
+  $('<div><p class="main-font">- ' + str + '</p></div>').addClass(btstrpClasses).appendTo('#main-container');
 }
